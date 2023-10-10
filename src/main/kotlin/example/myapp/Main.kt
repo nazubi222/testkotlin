807e24fe1f9e5e70a8ea97915f2dcdc2cfa2457c
Tùng Anh <tuganhhy@gmail.com>
2023-10-10 23:11:09 +0700

    anhtt

--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -1,5 +1,6 @@
 package example.myapp
 
+
 fun buildAquarium(){
     val aquarium = Aquarium(length = 25, width = 25, height = 40)
     aquarium.printSize()
@@ -8,16 +9,16 @@ fun buildAquarium(){
     //println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height /1000} liters")
 }
 
-fun makeFish(){
-    val shark = Shark()
-    val pleco = Plecostomus()
-    println("Shark: ${shark.color}")
-    shark.eat()
-    println("Plecostomus: ${pleco.color}")
-    pleco.eat()
-}
+//fun makeFish(){
+//    val shark = Shark()
+//    val pleco = Plecostomus()
+//    println("Shark: ${shark.color}")
+//    shark.eat()
+//    println("Plecostomus: ${pleco.color}")
+//    pleco.eat()
+//}
 
 fun main(){
 //    buildAquarium()
-    makeFish()
+//    makeFish()
 }
---
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -0,0 +1,17 @@
+package example.myapp.decor
+
+enum class Color(val rgb: Int){
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
+fun main() {
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+}
+
+
