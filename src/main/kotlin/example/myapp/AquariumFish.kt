ee946e93aed2a08c7dbd8a83b5d512650ab41de9
Tùng Anh <tuganhhy@gmail.com>
2023-10-10 22:39:30 +0700

    anhtt

---
+++ src/main/kotlin/example/myapp/AquariumFish.kt
@@ -0,0 +1,22 @@
+package example.myapp
+
+abstract class AquariumFish {
+    abstract val color: String
+}
+interface  FishAction{
+    fun eat()
+}
+
+class Shark: AquariumFish(), FishAction{
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus: AquariumFish(), FishAction{
+    override val color = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
+}
