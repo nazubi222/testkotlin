0a19e55b1df503d2bcb12489052341603e8372f3
Tùng Anh <tuganhhy@gmail.com>
2023-10-10 22:39:22 +0700

    anhtt

--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -1,13 +1,23 @@
 package example.myapp
 
 fun buildAquarium(){
-    val aquarium6 = Aquarium(numberofFish =  29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+    val aquarium = Aquarium(length = 25, width = 25, height = 40)
+    aquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
     //println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height /1000} liters")
 }
 
+fun makeFish(){
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
+
 fun main(){
-    buildAquarium()
+//    buildAquarium()
+    makeFish()
 }
