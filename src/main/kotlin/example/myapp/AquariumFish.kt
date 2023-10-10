96df66df4d6b1405941a46a9a9198d0734f85836
Tùng Anh <tuganhhy@gmail.com>
2023-10-10 22:46:50 +0700

    anhtt

--- src/main/kotlin/example/myapp/AquariumFish.kt
+++ src/main/kotlin/example/myapp/AquariumFish.kt
@@ -1,22 +1,28 @@
 package example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
+interface FishColor{
+    val color: String
 }
 interface  FishAction{
     fun eat()
 }
+object  GoldColor : FishColor{
+    override val color = "gold"
+}
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
 
-class Shark: AquariumFish(), FishAction{
+class Shark: FishColor, FishAction{
     override val color = "grey"
     override fun eat() {
         println("hunt and eat fish")
     }
 }
 
-class Plecostomus: AquariumFish(), FishAction{
-    override val color = "gold"
-    override fun eat() {
-        println("eat algae")
-    }
-}
+class Plecostomus (fishColor: FishColor = GoldColor):
+    FishAction by PrintingFishAction("eat algae"),
+    FishColor by fishColor
+
